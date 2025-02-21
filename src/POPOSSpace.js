fb2e8542b77aa13cc8c3e81074500e25b8457143
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-21 01:09:07 -0800

    using props

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-function POPOSSpace() {
+function POPOSSpace(props) {
+    const { name, image, address } = props;  // Destructure props
     return (
       <div>
         <img
-          src={`${process.env.PUBLIC_URL}/images/50-california-st.jpg`}
+          src={`${process.env.PUBLIC_URL}/images/${image}`}
           width="300"
           height="300"
-          alt="50 California St."
+          alt={name}
         />
-        <h1>Name...</h1>
-        <div>Address...</div>
+        <h1>{name}</h1>
+        <div>{address}</div>
       </div>
     );
-  }
+  }  
 
 export default POPOSSpace;
