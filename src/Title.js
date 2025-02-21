29b788458914aae7f205ccc3dd32fb14636e58f5
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-21 00:49:01 -0800

    added subtext

--- src/Title.js
+++ src/Title.js
@@ -3,8 +3,9 @@ import './Title.css';
 
 function Title() {
   return (
-    <div className='Title'>
+    <div className="Title">
       <h1>SFPOPOS</h1>
+      <div className="Title-Subtitle">San Francisco's Privately Owned Public Spaces</div>
     </div>
   );
 }
