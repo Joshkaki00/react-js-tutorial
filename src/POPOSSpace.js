eb8b07ddbcba1b6c5b8ec96443c13a60363cedbe
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-23 13:11:02 -0800

    added feature availability

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -7,14 +7,16 @@ function POPOSSpace(props) {
   return (
     <div className="POPOSSpace">
       <h1>{name}</h1>
-      <img
-        src={`${process.env.PUBLIC_URL}/images/${image}`}
-        width="300"
-        height="300"
-        alt={name}
-      />
-      <div>{address}</div>
-      <div className="POPOSSpace-Hours">{hours}</div>
+      {image && (
+        <img
+          src={`${process.env.PUBLIC_URL}/images/${image}`}
+          width="300"
+          height="300"
+          alt={name}
+        />
+      )}
+      {address && <div>{address}</div>}
+      {hours && <div className="POPOSSpace-Hours">{hours}</div>}
       {website && (
         <div className="POPOSSpace-Link">
           <a href={website} target="_blank" rel="noopener noreferrer">Visit Website</a>
