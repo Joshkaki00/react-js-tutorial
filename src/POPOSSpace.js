13639bfe72d8c4947a98c3ebe3d21f1e3442a48e
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-22 22:36:35 -0800

    updated popospace

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './POPOSSpace.css';
 
 function POPOSSpace(props) {
-    const { name, image, address } = props;  // Destructure props
-    return (
-      <div>
-        <img
-          src={`${process.env.PUBLIC_URL}/images/${image}`}
-          width="300"
-          height="300"
-          alt={name}
-        />
-        <h1>{name}</h1>
-        <div>{address}</div>
-      </div>
-    );
-  }  
+  const { name, image, address, hours } = props;
+
+  return (
+    <div className="POPOSSpace">
+      <h1>{name}</h1>
+      <img
+        src={`${process.env.PUBLIC_URL}/images/${image}`}
+        width="300"
+        height="300"
+        alt={name}
+      />
+      <div>{address}</div>
+      <div className="POPOSSpace-Hours">{hours}</div>
+    </div>
+  );
+}
 
 export default POPOSSpace;
