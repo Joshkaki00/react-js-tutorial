93e3489783ef945da3015bea8b980f7175d696f9
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-23 13:15:40 -0800

    updated about page

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About SFPOPOS</h1>
+      <p>POPOS are publicly accessible spaces in the forms of plazas, terraces, atriums, and parks, maintained by private developers in San Francisco.</p>
+    </div>
+  );
+}
+
+export default About;
