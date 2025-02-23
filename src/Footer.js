0012c9e46639fb38dfd4b3c78e2d9c4bb55d7fc7
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-22 22:29:05 -0800

    added links to footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './Footer.css';  // Import styles
 
 function Footer() {
-  return (
-    <div className="Footer">
-      <p>Mitchell Hudson &copy; {new Date().getFullYear()}</p>
-    </div>
-  );
-}
-
-export default Footer;
+    const currentYear = new Date().getFullYear();
+  
+    return (
+      <div className="Footer">
+        <p>Mitchell Hudson &copy; {currentYear}</p>
+        <nav className="Footer-Nav">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default Footer;
