c0ed060eda9514b4a2a693879d2e4b6b220497ac
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-23 13:25:12 -0800

    updated list js

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -4,15 +4,14 @@ import './POPOSList.css';
 import data from './sfpopos-data.json';
 
 function POPOSList() {
-  const spaces = data.map(({ title, address, images, hours, website, features }) => (
+  const spaces = data.map(({ title, address, images, hours }, i) => (
     <POPOSSpace
+      id={i}  // Pass the index as the id
       key={title}
       name={title}
       address={address}
       image={images[0]}
       hours={hours}
-      website={website}
-      features={features}
     />
   ));
 
