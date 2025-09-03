33f5b8f4b829689e6f92af5bdf09b20912097ebf
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-09-02 18:00:56 -0700

    responsive for now

--- src/components/POPOSDetails.js
+++ src/components/POPOSDetails.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import data from '../sfpopos-data.json';
+import './POPOSDetails.css';
 
 function POPOSDetails() {
   const { id } = useParams();  // Get the ID from the URL
   const { images, title, desc, hours, features, geo } = data[id];  // Get the data using ID
 
   return (
-    <div>
-      <div>
+    <div className="POPOSDetails">
+      <div className="POPOSDetails-image">
         <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt={title} />
       </div>
-      <div>
+      <div className="POPOSDetails-content">
         <h1>{title}</h1>
         <p>{desc}</p>
-        <p>{hours}</p>
-        <p>{features.join(', ')}</p>
-        <p>{geo.lat}, {geo.lon}</p>
+        {hours && <p className="POPOSDetails-hours"><strong>Hours:</strong> {hours}</p>}
+        {features && features.length > 0 && (
+          <p className="POPOSDetails-features"><strong>Features:</strong> {features.join(', ')}</p>
+        )}
+        <p className="POPOSDetails-coordinates"><strong>Coordinates:</strong> {geo.lat}, {geo.lon}</p>
       </div>
     </div>
   );
